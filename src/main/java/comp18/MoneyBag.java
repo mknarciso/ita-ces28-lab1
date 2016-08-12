81d9e38ba2a4bf24a7c104ba16a69f49426cfc5f
mknarciso <murilokn@gmail.com>
2016-08-12 16:09:27 +0000

    Step 8

--- src/main/java/comp18/MoneyBag.java
+++ src/main/java/comp18/MoneyBag.java
@@ -4,29 +4,45 @@ import java.util.*;
 public class MoneyBag {
     private List<Money> _wallet = new ArrayList<Money>();
     
-    public boolean hasCurrency(Currency currency){
+    public boolean hasCurrency(String currencyCode){
         for(int i=0;i < _wallet.size();i++){
-            if(_wallet.get(i).getCurrency()==currency)
+            if(_wallet.get(i).getCurrency().getCode()==currencyCode)
                 return true;
         }
         return false;
     }
     
-    /*public int currencyPosition(Currency currency){
-        for(int i=0;i < _types.size();i++){
-            if(_types.get(i).getCurrency()==currency)
+    public int currencyPosition(String currencyCode){
+        for(int i=0;i < _wallet.size();i++){
+            if(_wallet.get(i).getCurrency().getCode()==currencyCode)
                 return i;
         }
         return -1;
-    }*/
+    }
 
     
     public void add(Money newMoney){
-        this._wallet.add(newMoney);
+        String currencyCode = newMoney.getCurrency().getCode();
+        if(this.hasCurrency(currencyCode)){
+            int position = currencyPosition(currencyCode);
+            _wallet.set(position,_wallet.get(position).add(newMoney));
+        } else {
+            _wallet.add(newMoney);
+        }
+
     }
     
     public int getAmount(String currencyCode){
-        _t
-        return 0;
+        if(this.hasCurrency(currencyCode)){
+            int total = 0;
+            for(int i=0;i < _wallet.size();i++){
+                if(_wallet.get(i).getCurrency().getCode()==currencyCode)
+                    total = total + _wallet.get(i).getAmount();
+            }
+            return total;
+        } else{
+            return 0;
+        }
     }
+
 }
